2e996eebd4ca3c006f3cce62a53cba0a3fce975d
SoftwareBlair <br@ndon.dev>
2023-11-28 08:16:48 -0700

    Adding GitHub Desktop option

--- prompts/DevToolsPrompt.js
+++ prompts/DevToolsPrompt.js
@@ -1,31 +1,35 @@
 export const DevToolsPrompt = {
-  type: "checkbox",
-  name: "installDevTools",
-  message: "Which dev tools would you like to install?",
+  type: 'checkbox',
+  name: 'installDevTools',
+  message: 'Which dev tools would you like to install?',
   choices: [
     {
-      name: "Warp",
-      value: "warp",
+      name: 'Warp',
+      value: 'warp',
     },
     {
-      name: "Iterm2",
-      value: "iterm2",
+      name: 'Iterm2',
+      value: 'iterm2',
     },
     {
-      name: "Hyper",
-      value: "hyper",
+      name: 'Hyper',
+      value: 'hyper',
     },
     {
-      name: "Raycast",
-      value: "raycast",
+      name: 'Raycast',
+      value: 'raycast',
     },
     {
-      name: "VS Code",
-      value: "visual-studio-code",
+      name: 'VS Code',
+      value: 'visual-studio-code',
     },
     {
-      name: "Docker",
-      value: "docker",
+      name: 'Docker',
+      value: 'docker',
     },
+    {
+      name: 'Github Desktop',
+      value: 'github',
+    }
   ]
 };
